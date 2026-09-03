0d51a1ec7dc127fcd371830d5d07bc04bb843a01
Mazhar Ali <bsf23005103@ue.edu.pk>
2026-09-03 11:06:38 +0000

    learned variables

---
+++ 01_basics/1_variables.js
@@ -0,0 +1,16 @@
+const accountId = 110;
+let accountEmail = "alpha@google.com";
+var accountPassword = "12345"
+accountCity = "Pindi"
+accountState;
+// accountId = 10;
+
+console.log(accountId);
+
+accountEmail = "ab@cd.com"
+accountPassword = "00000"
+accountCity = "Isb"
+
+console.table([accountId, accountEmail, accountPassword, accountCity,])
+
+// node 01_basics/1_variables.js
