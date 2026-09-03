f80c4e1cf243e4a8bcea2e6e164414c303463646
Mazhar Ali <bsf23005103@ue.edu.pk>
2026-09-03 13:18:53 +0000

    Conversion of datatypes

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = "50"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+ 
+
+// node 01_basics/3_conversionOperation.js
