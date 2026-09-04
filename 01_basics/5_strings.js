37bbe47cbafc7246d08d11070a0434e569c8a8b4
Mazhar Ali <bsf23005103@ue.edu.pk>
2026-09-04 13:17:42 +0000

    strings and functions

---
+++ 01_basics/5_strings.js
@@ -0,0 +1,19 @@
+const name = "Maz"
+const age = 20
+
+// console.log(name + age + " is Me");
+
+console.log(`Hello myself ${name} and I'm ${age} years old`)
+
+const gameName = new String('He-man')
+
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+
+
+
+
+//  node 01_basics/5_strings.js
