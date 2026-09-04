88a8a874310c400f15e37b6c3e5fa64e1729500d
Mazhar Ali <bsf23005103@ue.edu.pk>
2026-09-04 07:04:42 +0000

    operations

--- 01_basics/3_conversionOperation.js
+++ 01_basics/3_conversionOperation.js
@@ -10,6 +10,33 @@ console.log(valueInNumber);
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
- 
+
+let value = 3
+let negvalue = -value
+
+console.log(negvalue);
+
+// *********************  OPERATIONS  *********************
+
+let str1 = "Hello"
+let str2 = " HR"
+
+let str3 = (str1 + str2)
+console.log(str3);
+
+console.log(1 + 2);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
 
 // node 01_basics/3_conversionOperation.js
