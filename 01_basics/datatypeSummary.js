5abaa345e5d538a21ab924cdad23c1e2b568f37b
Mazhar Ali <bsf23005103@ue.edu.pk>
2026-09-04 10:02:09 +0000

    datatypes summary

---
+++ 01_basics/datatypeSummary.js
@@ -0,0 +1,41 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+// console.log(userEmail);
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+console.log(typeof anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const arr_heroes = ["Commandar Safeguard", "Burka Avenger", "Captain Pakistan"];
+
+let myObj = {
+    name: "abcd",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+//  node 01_basics/datatypeSummary.js
