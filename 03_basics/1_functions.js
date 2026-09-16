784b572e6bf2b637393ab98cd086d11c79e76e9c
Mazhar Ali <bsf23005103@ue.edu.pk>
2026-09-16 14:07:45 +0000

    functions

---
+++ 03_basics/1_functions.js
@@ -0,0 +1,17 @@
+function sayMyName(){
+    console.log("M");
+    console.log("A");
+    console.log("A");
+    console.log("H");
+    console.log("I");
+}
+
+// sayMyName()
+
+
+
+
+
+
+
+// node 03_basics/1_functions.js
