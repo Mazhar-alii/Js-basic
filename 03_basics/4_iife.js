517e39b6e653a93d9f02329b4df9ed91aa535c5a
Mazhar Ali <bsf23005103@ue.edu.pk>
2026-09-22 10:39:09 +0000

    iife

---
+++ 03_basics/4_iife.js
@@ -0,0 +1,25 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named iife
+    console.log(`DB connected`);
+})();
+
+( (name) => {
+    // simple iife + parameter use
+    console.log(`DB two connected ${name}`);
+} )("Memes")
+
+
+
+
+
+
+
+
+
+
+
+
+// node 03_basics/4_iife.js
