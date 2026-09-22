37a0e5e78e25ffd19ace9c8f0cca365751858c80
Mazhar Ali <bsf23005103@ue.edu.pk>
2026-09-22 14:00:38 +0000

    if , else if , nested

---
+++ 04_control_flow/one.js
@@ -0,0 +1,55 @@
+// if
+
+const isUserLoggedIn = true
+const temperature = 20
+
+// if (temperature >= 30) {
+//     console.log("It's a really hot weather");
+// } else if (temperature <= 4 ){
+//     console.log("It's a really cold weather");
+// } else {
+//     console.log("It's a really beautiful weather");
+// }
+
+
+// const score = 200
+
+// if (score >= 100) {
+//     const power = "fly"
+//     console.log(`Player new power : ${power}`);
+// }
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course");
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in");
+// }
+
+
+
+// node 04_control_flow/one.js
